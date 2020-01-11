229d8ae0cfc264a0b0d10ceb74e8af6bed615078
darkninja51 <54821095+darkninja51@users.noreply.github.com>
2020-01-11 03:13:20 +0200

    Update code.lua

--- code.lua
+++ code.lua
@@ -1,7 +1,9 @@
 Citizen.CreateThread(function()
    Citizen.Wait(3000)
-      local auth = false
+      local auth = true
    if not auth then
        print('SERVER AUTHENTICATION FAILED. STOLEN SERVER! STOPPING....')
+   else
+      print('SERVER AUTHENTICATION SUCCESS. HELLO OWNER HEISENBERG691#1288')
    end
 end)
