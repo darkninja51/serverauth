6dc4ffb0afc491e6ca139de68ca08262bf1961ed
darkninja51 <54821095+darkninja51@users.noreply.github.com>
2020-01-11 02:49:17 +0200

    Update code.lua

--- code.lua
+++ code.lua
@@ -1,12 +1,10 @@
 local auth = false
-if not auth then
+
 	
 	print('SERVER AUTHENTICATION FAILED. STOLEN SERVER! STOPPING....')
-	StopResource('es_extended')
-	Citizen.CreateThread(function()
-		while true do
-		end
-	end)
-else
-	print('AUTH:'..auth)
-end
+--	StopResource('es_extended')
+--	Citizen.CreateThread(function()
+--		while true do
+--		end
+--	end)
+
