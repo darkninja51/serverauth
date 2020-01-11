74bd6ca2e08206d161700f5d5041fc41dc220c20
darkninja51 <54821095+darkninja51@users.noreply.github.com>
2020-01-11 03:21:44 +0200

    Update code.lua

--- code.lua
+++ code.lua
@@ -3,9 +3,7 @@ Citizen.CreateThread(function()
       local auth = false
    if auth == false then
        print('SERVER AUTHENTICATION FAILED. STOLEN SERVER! STOPPING....')
-         print(auth)
    else
       print('SERVER AUTHENTICATION SUCCESS. HELLO OWNER HEISENBERG691#1288')
-         print('AUTH'..auth)
    end
 end)
